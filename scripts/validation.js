5ec5b06b3d4137889160bf7d86667e36c74ec2a3
Mason Serna <maserna23@gmail.com>
2024-08-13 19:38:29 -0400

    Add enableValidation, setEventListeners, checkInputValidity, and showInputError functions

---
+++ scripts/validation.js
@@ -0,0 +1,38 @@
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorMessageID = inputElement.id + "-error";
+  const errorMessageElement = document.querySelector("#" + errorMessageID);
+  errorMessageElement.textContent = errorMessage;
+  console.log(errorMessageID);
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement, inputElement.validationMessage);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".modal__input"));
+  const buttonElement = formElement.querySelector(".modal__submit-btn");
+
+  //TO DO- handle initial states
+  // toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+      // toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".modal__form"));
+  formList.forEach((formElement) => {
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
